11ed051d6ecbc8bc4d79f9cd0ae6dd28a4d4edcd
Hyde Yoo <hydeyoo0716@gmail.com>
2026-05-10 16:48:20 -0400

    feat: migrate more config to home manager

--- home.nix
+++ home.nix
@@ -11,8 +11,6 @@
         pkgs.gvfs
         pkgs.tumbler
         pkgs.fuzzel
-        pkgs.mako
-        pkgs.swaylock
         pkgs.thunar
         pkgs.xwayland-satellite # xwayland support
         pkgs.brightnessctl
@@ -20,15 +18,6 @@
         pkgs.firefox
         pkgs.zathura
       ];
-
-      # This value determines the Home Manager release that your configuration is
-      # compatible with. This helps avoid breakage when a new Home Manager release
-      # introduces backwards incompatible changes.
-      #
-      # You should not change this value, even if you update Home Manager. If you do
-      # want to update the value, then make sure to first check the Home Manager
-      # release notes.
-      home.stateVersion = "25.11"; # Please read the comment before changing.
 
       home.pointerCursor = {
         package = pkgs.adwaita-icon-theme;
@@ -55,16 +44,21 @@
           format = "ssh";
         };
       };
+
       programs.zsh = {
         enable = true;
         history.size = 10000;
       };
+
+      programs.zoxide.enable = true;
+
       programs.swaylock = {
         enable = true;
         settings = {
           color = "000000";
         };
       };
+
       services.swayidle =
         let
           lock = "${pkgs.swaylock}/bin/swaylock --daemonize";
@@ -101,6 +95,78 @@
           };
         };
 
+      programs.foot = {
+        enable = true;
+        server.enable = true;
+        settings = {
+          main = {
+            font = "Firacode Nerd Font:size=8";
+          };
+          cursor.style = "beam";
+          colors-dark = {
+            background = "#000000";
+            foreground = "#cccccc";
+
+            selection-foreground = "#c8c093";
+            selection-background = "#2d4f67";
+
+            regular0 = "#090618";
+            regular1 = "#c34043";
+            regular2 = "#76946a";
+            regular3 = "#c0a36e";
+            regular4 = "#7e9cd8";
+            regular5 = "#957fb8";
+            regular6 = "#6a9589";
+            regular7 = "#c8c093";
+
+            bright0 = "#727169";
+            bright1 = "#e82424";
+            bright2 = "#98bb6c";
+            bright3 = "#e6c384";
+            bright4 = "#7fb4ca";
+            bright5 = "#938aa9";
+            bright6 = "#7aa89f";
+            bright7 = "#dcd7ba";
+
+            "16" = "#ffa066";
+            "17" = "#ff5d62";
+          };
+        };
+      };
+
+      programs.fuzzel = {
+        enable = true;
+        settings = {
+          main = {
+            icons-enabled = false;
+          };
+          colors = {
+            background = "#080808dd";
+            text = "#bdbdbdff";
+            prompt = "#80a0ffff";
+            placeholder = "#cf87e8ff";
+            input = "#c6c6c6ff";
+            match = "#79dac8ff";
+            selection = "#323437aa";
+            selection-text = "#c6c6c6ff";
+            selection-match = "#36c692ff";
+            counter = "#cf87e8ff";
+            border = "#8cc85fff";
+          };
+        };
+      };
+
+      services.mako = {
+        enable = true;
+        settings = {
+          font = "Firacode Nerd Font 11";
+          default-timeout = 5000;
+          layer = "overlay";
+          background-color = "#000000";
+        };
+      };
+
+      # Theme settings
       dconf.settings = {
         "org/gnome/desktop/interface" = {
           color-scheme = "prefer-dark";
@@ -115,6 +181,7 @@
         };
         gtk4.theme = null;
       };
+
       qt = {
         enable = true;
         platformTheme.name = "adwaita";
@@ -123,9 +190,14 @@
           package = pkgs.adwaita-qt6;
         };
       };
-      programs.foot = {
-        enable = true;
-        server.enable = true;
-      };
+
+      # This value determines the Home Manager release that your configuration is
+      # compatible with. This helps avoid breakage when a new Home Manager release
+      # introduces backwards incompatible changes.
+      #
+      # You should not change this value, even if you update Home Manager. If you do
+      # want to update the value, then make sure to first check the Home Manager
+      # release notes.
+      home.stateVersion = "25.11"; # Please read the comment before changing.
     };
 }
